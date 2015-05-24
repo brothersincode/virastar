ebb116f2b9c7928b81bccfde4a34248a01d446a9
Nasser Rafie <contact@geminorum.ir>
2015-05-24 17:11:05 +0430

    trying ZeroClipboard

---
+++ assets/js/main.js
@@ -0,0 +1,43 @@
+
+window.onload=function(){
+
+	virastar=new Virastar();
+	var input=document.getElementById('textarea');
+	var output=document.getElementById('plain');
+
+	function viraiesh(){
+	    output.innerHTML=virastar.cleanup(input.value).replace(/\n/g,"</br>")
+
+	}
+	viraiesh();
+	document.getElementById('submit').onclick=viraiesh;
+	input.onchange=viraiesh;
+	input.onkeyup = viraiesh;
+
+
+	ZeroClipboard.config( {
+		// swfPath: "https://rawgit.com/juvee/virastar/gh-pages/assets/packages/zeroclipboard/ZeroClipboard.swf"
+		swfPath: "https://rawgit.com/zeroclipboard/zeroclipboard/master/dist/ZeroClipboard.swf"
+	} );
+
+	var ZClient = new ZeroClipboard( document.getElementById("copy_button") );
+
+	// ZClient.on( "copy", function (event) {
+	//   var clipboard = event.clipboardData;
+	//   clipboard.setData( "text/plain", "Copy me!" );
+	//   clipboard.setData( "text/html", "<b>Copy me!</b>" );
+	//   clipboard.setData( "application/rtf", "{\\rtf1\\ansi\n{\\b Copy me!}}" );
+	// });
+
+	ZClient.on( "ready", function( readyEvent ) {
+		console.log( "ZeroClipboard SWF is ready!" );
+
+		ZClient.on( "aftercopy", function( event ) {
+			// `this` === `client`
+			// `event.target` === the element that was clicked
+			event.target.style.display = "none";
+			console.log("Copied text to clipboard: " + event.data["text/plain"] );
+		} );
+	} );
+
+}
