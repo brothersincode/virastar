f88185dd64781ca3a77382ea04f5252879eece7f
Nasser Rafie <contact@geminorum.ir>
2016-12-04 05:58:41 +0330

    refreshing
    + using clipboard.js
    + full height on firefox/chrome/opera
    + no script notice
    + skipping markdown options

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,45 @@
+window.onload = function() {
 
-window.onload=function(){
+	virastar = new Virastar({skip_markdown_ordered_lists_numbers_conversion: false});
 
-	virastar=new Virastar();
-	var input=document.getElementById('textarea');
-	var output=document.getElementById('plain');
+	var input = document.getElementById('textarea');
+	var output = document.getElementById('plain');
 
-	function viraiesh(){
-	    output.innerHTML=virastar.cleanup(input.value).replace(/\n/g,"</br>")
-
+	function viraiesh() {
+		output.innerHTML = virastar.cleanup(input.value).replace(/\n/g, "</br>")
 	}
+
 	viraiesh();
-	document.getElementById('submit').onclick=viraiesh;
-	input.onchange=viraiesh;
+	// document.getElementById('submit').onclick = viraiesh;
+	input.onchange = viraiesh;
 	input.onkeyup = viraiesh;
 
+	var clipboard = new Clipboard('.copy');
 
-	ZeroClipboard.config( {
-		trustedDomains: [
-			"juvee.github.io"
-		],
-		swfPath: "http://rawgit.com/zeroclipboard/zeroclipboard/master/dist/ZeroClipboard.swf"
-	} );
+	clipboard.on('success', function(e) {
+		console.log(e);
+	});
 
-	var ZClient = new ZeroClipboard( document.getElementById("copy_button") );
+	clipboard.on('error', function(e) {
+		console.log(e);
+	});
 
-	ZClient.on( "ready", function( readyEvent ) {
-		console.log( "ZeroClipboard SWF is ready!" );
-
-		ZClient.on('copy', function(event) {
-		    var text = document.getElementById('plain').innerHTML;
-		    // var windowsText = text.replace(/\n/g, '\r\n');
-		    var windowsText = text.replace(/<br\s*[\/]?>/gi, '\r\n');
-		    event.clipboardData.setData('text/plain', windowsText);
-		});
-
-		ZClient.on( "aftercopy", function( event ) {
-			console.log("Copied text to clipboard: " + event.data["text/plain"] );
-		} );
-	} );
+	// ZeroClipboard.config({trustedDomains: ["juvee.github.io"], swfPath: "http://rawgit.com/zeroclipboard/zeroclipboard/master/dist/ZeroClipboard.swf"});
+	//
+	// var ZClient = new ZeroClipboard(document.getElementById("copy_button"));
+	//
+	// ZClient.on("ready", function(readyEvent) {
+	// 	console.log("ZeroClipboard SWF is ready!");
+	//
+	// 	ZClient.on('copy', function(event) {
+	// 		var text = document.getElementById('plain').innerHTML;
+	// 		// var windowsText = text.replace(/\n/g, '\r\n');
+	// 		var windowsText = text.replace(/<br\s*[\/]?>/gi, '\r\n');
+	// 		event.clipboardData.setData('text/plain', windowsText);
+	// 	});
+	//
+	// 	ZClient.on("aftercopy", function(event) {
+	// 		console.log("Copied text to clipboard: " + event.data["text/plain"]);
+	// 	});
+	// });
 }
