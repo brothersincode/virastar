4bb608b071e0e12d7bc5b501883560496f8f172c
Hamed <hamed@localhost.localdomain>
2017-01-12 23:37:12 +0330

    :clap: unit testing with mocha

---
+++ test/unit_test.js
@@ -0,0 +1,23 @@
+var assert = require('assert');
+var Virastar = require('../lib/virastar.js')
+
+describe('Virastar.js', function() {
+	var virastar;
+	it('should create new instance', function() {
+		virastar = new Virastar();
+	});
+
+	describe('#cleanup()', function() {
+
+		// some missing tests
+		it('should skip non-written tests', function() {});
+
+
+		it('should replace kashidas to ndash in parenthetic', function() {
+			assert.equal(
+				virastar.cleanup("ـ که در واقع پدرخوانده‌ام بود ولی من او را جای پدرم می‌دانستم ـ"),
+				"– که در واقع پدرخوانده‌ام بود ولی من او را جای پدرم می‌دانستم –"
+			);
+		});
+	});
+});
