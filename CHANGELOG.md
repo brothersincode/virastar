4cc02f6a9a2b90033d6de64fcd433fe1da046d15
Nasser Rafie <contact@geminorum.ir>
2018-02-10 22:38:56 +0330

    :package: 0.13.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,22 +1,24 @@
-## 0.12.0
-### Added
+### 0.13.0
+- Added: [coding standard](https://github.com/Flet/semistandard)
+- Added: passing options directly into cleanup method
+
+### 0.12.0
+##### Added
 - new option: `preserve_brackets`
 - new option: `preserve_braces`
 
-
-## 0.11.0
-### Added
+### 0.11.0
+##### Added
 - new option: kashidas_as_parenthetic.
 
-### Fixed
+##### Fixed
 - remove all kashida between non-whitespace characters
 
-
-## 0.10.0
-### Added
+### 0.10.0
+##### Added
 - new word tokenizer detection.
 - skip english numbers conversion in html entities.
 - bower integration.
 
-### Fixed
+##### Fixed
 - fixed multiline flag on begin/end cleanup
--- test/unit_test.js
+++ test/unit_test.js
@@ -1,23 +1,24 @@
+/* global describe, it */
+
 var assert = require('assert');
-var Virastar = require('../lib/virastar.js')
+var Virastar = require('../lib/virastar.js');
 
-describe('Virastar.js', function() {
-	var virastar;
-	it('should create new instance', function() {
-		virastar = new Virastar();
-	});
+describe('Virastar.js', function () {
+  var virastar;
 
-	describe('#cleanup()', function() {
+  it('should create new instance', function () {
+    virastar = new Virastar();
+  });
 
-		// some missing tests
-		it('should skip non-written tests', function() {});
+  describe('#cleanup()', function () {
+    // some missing tests
+    it('should skip non-written tests', function () {});
 
-
-		it('should replace kashidas to ndash in parenthetic', function() {
-			assert.equal(
-				virastar.cleanup("ـ که در واقع پدرخوانده‌ام بود ولی من او را جای پدرم می‌دانستم ـ"),
-				"– که در واقع پدرخوانده‌ام بود ولی من او را جای پدرم می‌دانستم –"
-			);
-		});
-	});
+    it('should replace kashidas to ndash in parenthetic', function () {
+      assert.equal(
+        virastar.cleanup('ـ که در واقع پدرخوانده‌ام بود ولی من او را جای پدرم می‌دانستم ـ'),
+        '– که در واقع پدرخوانده‌ام بود ولی من او را جای پدرم می‌دانستم –'
+      );
+    });
+  });
 });
